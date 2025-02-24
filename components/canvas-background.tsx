a3ce139e45f11555d5c45cc36f23249f194be42f
dualitydomain <d3moni0bang@gmail.com>
2025-02-24 03:31:40 -0300

    Actualizar canvas-background.tsx

--- components/canvas-background.tsx
+++ components/canvas-background.tsx
@@ -1,11 +1,14 @@
 "use client"
 
-import { useEffect, useRef } from "react"
+import { useEffect } from "react"
+
+import { useRef } from "react"
+
 import * as THREE from "three"
-import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer"
-import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass"
-import { UnrealBloomPass } from "three/examples/jsm/postprocessing/UnrealBloomPass"
-import { ShaderPass } from "three/examples/jsm/postprocessing/ShaderPass"
+import { EffectComposer } from "three/addons/postprocessing/EffectComposer.js"
+import { RenderPass } from "three/addons/postprocessing/RenderPass.js"
+import { UnrealBloomPass } from "three/addons/postprocessing/UnrealBloomPass.js"
+import { ShaderPass } from "three/addons/postprocessing/ShaderPass.js"
 
 // Custom shader for the glitch effect
 const GlitchShader = {
@@ -260,4 +263,3 @@ export default function CanvasBackground() {
     />
   )
 }
-
